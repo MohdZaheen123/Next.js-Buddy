49360d5f79dc3e552e9243a846ac812b4ed684bc
Mohamed zaheen <mohamedzaheen986@gmail.com>
2023-11-02 17:48:42 +0530

    github initial phase

--- index.js
+++ index.js
@@ -7,17 +7,35 @@ import gradient from 'gradient-string'
 
 
 let projectname = '';
+let github_repo='';
+let Lucide='';
 
 async function askname() {
-  return inquirer.prompt({
-    name: 'Project_name',
-    type: 'input',
-    message: 'Would you like to install and setup next.js? (y/n)',
-  }).then(answer => {
+  return inquirer.prompt([{name: 'Project_name',type: 'confirm',message: 'Would you like to install and setup next.js ?'},
+        {type:'confirm', name:'github', message:'Would you like to connect to Github repo?'}, 
+        {type:'confirm', name:'lucide', message:'Would you like to setup Lucide react icons package?'},
+        // {type:'input', name:'email', message:'E-mail (for password recovering):', when:function(answers){return answers.protect_ask}},
+        // {type:'password', name:'password', message:'Password:', when:function(answers){return answers.protect_ask}},
+        // {type:'password', name:'confirm_password', message:'Confirm you password:', when:function(answers){return answers.protect_ask}}
+      ]).then(answer => {
     projectname = answer.Project_name;
+    if(!projectname){
+      process.exit(0)
+    }
+    github_repo=answer.github;
+    Lucide=answer.Lucide;
   });
 }
 
+
+
+// inquirer.prompt([
+//         {type:'input', name:'username', message:'Choose an username:'}, 
+//         {type:'confirm', name:'protect_ask', message:'Do you want to password-protect your game list?'},
+//         {type:'input', name:'email', message:'E-mail (for password recovering):', when:function(answers){return answers.protect_ask}},
+//         {type:'password', name:'password', message:'Password:', when:function(answers){return answers.protect_ask}},
+//         {type:'password', name:'confirm_password', message:'Confirm you password:', when:function(answers){return answers.protect_ask}}
+//     ])
 
 
 
@@ -53,6 +71,7 @@ async function main() {
     await intro();
     console.log(gradient.pastel.multiline('Create Next.js app in ease. Initializing Next.js app with Schnadcn UI!!'));
     await askname();
+    console.log(projectname)
     const success =await changeDirectoryAndCreateNextApp();
     if (success) {
       console.log('Directory changed, and Next.js app created successfully.');
